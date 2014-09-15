0eac57f6975e66911147d733cf0bbe9bdc101ba6
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2014-09-15 07:18:01 +0530

    resolving proxy issues

--- gulp/server.js
+++ gulp/server.js
@@ -8,19 +8,19 @@ var httpProxy = require('http-proxy');
 /* This configuration allow you to configure browser sync to proxy your backend */
 var proxyTarget = 'http://server/context/'; // The location of your backend
 
-var proxy = httpProxy.createProxyServer({
-  target: proxyTarget
-});
+// var proxy = httpProxy.createProxyServer({
+//   target: proxyTarget
+// });
 
 /* proxyMiddleware forwards static file requests to BrowserSync server
    and forwards dynamic requests to your real backend */
-function proxyMiddleware(req, res, next) {
-  if (/\.(html|css|js|png|jpg|jpeg|gif|ico|xml|rss|txt|eot|svg|ttf|woff)(\?((r|v|rel|rev)=[\-\.\w]*)?)?$/.test(req.url)) {
-    next();
-  } else {
-    proxy.web(req, res);
-  }
-}
+// function proxyMiddleware(req, res, next) {
+//   if (/\.(html|css|js|png|jpg|jpeg|gif|ico|xml|rss|txt|eot|svg|ttf|woff)(\?((r|v|rel|rev)=[\-\.\w]*)?)?$/.test(req.url)) {
+//     next();
+//   } else {
+//     proxy.web(req, res);
+//   }
+// }
 
 function browserSyncInit(baseDir, files, browser) {
   browser = browser === undefined ? 'default' : browser;
@@ -28,8 +28,8 @@ function browserSyncInit(baseDir, files, browser) {
   browserSync.instance = browserSync.init(files, {
     startPath: '/index.html',
     server: {
-      baseDir: baseDir,
-      middleware: proxyMiddleware
+      baseDir: baseDir
+      // middleware: proxyMiddleware
     },
     browser: browser
   });
